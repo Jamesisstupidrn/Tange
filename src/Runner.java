862a06d16c045cfb073a856226745d2764f06ceb
BT_1N3_10 <sample@bths.edu>
2024-10-25 13:09:05 -0400

    added some logic

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,15 @@
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello CSA");
+        System.out.println("What's up");
+        int x = 5;
+        x += 10;
+        x++;
+        System.out.println("x is now " +x);
+        if (x > 10) {
+            System.out.println("boop");
+        } else {
+            System.out.println("beep");
+        }
     }
 }
